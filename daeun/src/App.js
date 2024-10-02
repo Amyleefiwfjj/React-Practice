d25f18aa686ae93fd2543f2daec4c15bbd33f924
이다은 <162148102+Amyleefiwfjj@users.noreply.github.com>
2024-10-02 19:19:22 +0900

    app.js & component added

--- daeun/src/App.js
+++ daeun/src/App.js
@@ -1,25 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// //components없이 작성한 코드
+// function App() {
+//   return (
+// 	  <div>
+// 	    <div>
+// 		    <h3>React-Study</h3>
+// 		    <p>React Study 실습 / 과제 레포</p>
+// 		    <p>사용언어: JavaScript</p>
+// 	    </div>
+// 	    <div>
+// 		    <h3>Bridge</h3>
+// 		    <p>Bridge.. 어쩌구 내용</p>
+// 		    <p>사용언어: JavaScript</p>
+// 	    </div>
+// 	    <div>
+// 		    <h3>Spring-Study</h3>
+// 		    <p>Spring Study.. 어쩌구 내용</p>
+// 		    <p>사용언어: Java</p>
+// 	    </div>
+// 	    {/*위에 3뭉탱이가 반복됨 - 아 저걸 component로 만들면 한 코드를 재사용하면 되겠구나~!*/}
+//     </div>
+//   );
+// }
+
+//
 
 export default App;
